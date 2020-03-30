2060f16fd6649ad0781873efb2342ce185acfda7
thanhgianga303 <45537576+thanhgianga303@users.noreply.github.com>
2020-03-30 21:17:24 +0700

    test thu

--- HotelReservationWebsite/Models/Address.cs
+++ HotelReservationWebsite/Models/Address.cs
@@ -1,6 +1,6 @@
 namespace HotelReservationWebsite.Models
 {
-    public class Addr111ess
+    public class Address
     {
         public int AddressID { get; set; }
         public string AddressName { get; set; }
