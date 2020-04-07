9fede220f3c12cc6b5967d98695f6d14f929a37d
thanhgianga303 <45537576+thanhgianga303@users.noreply.github.com>
2020-04-07 17:52:42 +0700

    FrontEnd

---
+++ HotelReservationWebsite/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservationWebsite.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Hotel()
+        {
+            return View();
+        }
+        public IActionResult Account()
+        {
+            return View();
+        }
+        public IActionResult Promotion()
+        {
+            return View();
+        }
+        public IActionResult BookingDetails()
+        {
+            return View();
+        }
+        public IActionResult RoomCagotery()
+        {
+            return View();
+        }
+        public IActionResult Role()
+        {
+            return View();
+        }
+        public IActionResult Address()
+        {
+            return View();
+        }
+        public IActionResult City()
+        {
+            return View();
+        }
+        public IActionResult Customer()
+        {
+            return View();
+        }
+        public IActionResult Employee()
+        {
+            return View();
+        }
+    }
+}
