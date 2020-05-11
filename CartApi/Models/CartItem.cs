8ad4839fe20d203f59f1e20c84438e0e482ed12b
thanhgianga303 <45537576+thanhgianga303@users.noreply.github.com>
2020-05-12 00:34:42 +0700

    redis

--- CartApi/Models/CartItem.cs
+++ CartApi/Models/CartItem.cs
@@ -3,10 +3,19 @@ namespace CartApi.Models
     public class CartItem
     {
         public string Id { get; set; }
-        public string ProductId { get; set; }
-        public string ProductName { get; set; }
-        // public decimal UnitPrice { get; set; }
-        // public int Quantity { get; set; }
-        // public string PictureUri { get; set; }
+        public int HotelId { get; set; }
+        public string HotelName { get; set; }
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+        public int AddressId { get; set; }
+        public string HotelAddress { get; set; }
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int numberOfRooms {get; set;}
+        public int numberOfStandardRooms{get;set;}
+        public int numberOfSuperiorRooms{get;set;}
+        public int numberOfDeluxeRooms{get;set;}
+        public int numberOfSuiteRooms{get;set;}
     }
 }
